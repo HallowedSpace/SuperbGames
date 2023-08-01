a54b223475afb405846283c871b9859c41ce2b6b
Hallowed <124842334+HallowedSpace@users.noreply.github.com>
2023-08-01 18:46:05 -0400

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,50 @@
 //GEBI
 function g(e){
-    return document.getElementById(e);
+    let o = document.getElementById(e);
+    if(o){
+        return document.getElementById(e);
+    }else{
+        return undefined;
+    }
+}
+//functions
+function stage2Variables(){
+    window.sSwitch = g('settingsSwitch');
+    window.s_sBar = g('settingsSidebar');
+}
+function stage2(){
+    document.body.innerHTML = ``;
+    stage2Variables();
+    break;
 }
 //variables
-let article = g('article');
+//global
+const welcomeMsg = ["Welcome!", "Hello there!", "Greetings!", "Salutations!", "Nice to see ya!", "Glad you could make it!", "Enjoy!", "Have a good time!", "Hope you have a good time!"];
+let settingsOpen = 0;
+//stage1
+let article_S1 = g('article-stage1');
+let topbar_S1 = g('topbar-stage1');
+let body_S1 = g('body-stage1');
+//stage 2
 
+
+//buttons
+if(sSwitch){
+sSwitch.onclick = function () {
+    if (settingsOpen === 0) {
+      sSwitch.style.transform = "rotate(360deg)";
+      sSwitch.style.marginLeft = "190px";
+      settingsOpen++;
+    } else {
+      sSwitch.style.transform = "rotate(0deg)";
+      sSwitch.style.marginLeft = "0";
+      settingsOpen = 0;
+    };
+  };
+};
 //event listener
 document.addEventListener("keydown", (event) => {
     if(event.keyCode === 81 && event.ctrlKey){
-        alert("sus");
+        stage2();
     }
 })
