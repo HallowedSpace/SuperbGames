9910d5c53443cf158f72a270527300bbe799b3de
Hallowed <124842334+HallowedSpace@users.noreply.github.com>
2023-08-14 10:44:46 -0400

    update info

--- assets/js/test.js
+++ assets/js/test.js
@@ -7,11 +7,20 @@ function g(e){
         return undefined;
     }
 }
+//elements
 var sSw = g('settingsSwitch');
 var sSb = g('settingSidebar');
 var sCon = g('settingsContainer');
+var info = g('infoSVG');
+var exitInfo = g('exitInfo');
+var infoPopup = g('infoContainer');
+
+//other
+let infoOpen = false;
 let settingsOpen = 0;
-sSw.onclick = function () {
+
+//settings button
+sSw.onclick = () => {
     if (settingsOpen === 0) {
       document.querySelector('body').style.overflow = "hidden";
       sSw.style.transform = "rotate(360deg)";
@@ -28,3 +37,21 @@ sSw.onclick = function () {
         settingsOpen = 0;
     }
   };
+//info button
+info.onclick = () => {
+    if(settingsOpen = 1){
+        document.querySelector('body').style.overflow = "visible";
+        sSw.style.transform = "rotate(0deg)";
+        sSw.style.marginLeft = "0";
+        sSb.style.width = "0";
+        sCon.style.opacity = 0;
+        settingsOpen = 0;
+    }
+    infoPopup.style.display = "flex";
+    infoOpen = true;
+}
+
+exitInfo.onclick = () => {
+    infoPopup.style.display = "none";
+    infoOpen = false;
+}
