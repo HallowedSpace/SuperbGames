a4a2326ad0933fe387e50e3fabbfd05d9f9ce01a
Hallowed <124842334+HallowedSpace@users.noreply.github.com>
2023-08-31 18:36:36 -0400

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -731,24 +731,3 @@ document.addEventListener("keydown", (event) => {
         }
     }
 });
-//disable inspect element
-let removeThisToDisableInspectElement = `document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-});
-document.onkeydown = (e) => {
-    if (e.key == 123) {
-        e.preventDefault();
-    }
-    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
-        e.preventDefault();
-    }
-    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
-        e.preventDefault();
-    }
-    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
-        e.preventDefault();
-    }
-    if (e.ctrlKey && e.key == 'U') {
-        e.preventDefault();
-    }
-};`//remove this to disable inspect element (enabled for development purposes)
